a44d3abd1098a6976691b0a87cb8e0c4e0a106dd
podhmo <ababjam61+github@gmail.co.jp>
2025-06-03 12:22:40 +0900

    fix: loader needs fset

--- internal/loader/loader.go
+++ internal/loader/loader.go
@@ -8,8 +8,7 @@ import (
 )
 
 // LoadFile parses the given Go source file and returns its AST.
-func LoadFile(filename string) (*ast.File, error) {
-	fset := token.NewFileSet()
+func LoadFile(fset *token.FileSet, filename string) (*ast.File, error) {
 	file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse file %s: %w", filename, err)
